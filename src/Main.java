8e5ee6825e1f6b1a8f51a88e23753028bd7827d6
Duygu Sadak <duyguercan2009@gmail.com>
2024-11-27 18:23:03 +0200

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static final List<InventoryItem> inventory = FileIO.loadFile();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        listItems();
+    }
+
+    private static void listItems() {
+
+        if (inventory.isEmpty()) {
+            System.out.println("No items in inventory.");
+        } else {
+            for (InventoryItem item : inventory) {
+                System.out.println(item.getItemDetails());
+            }
+        }
     }
 }
